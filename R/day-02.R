87c1677090da9ffb5155a59d45b347076375f98d
Martin Petr <git@bodkan.net>
2021-12-02 12:13:05 +0100

    Rearrange submarine 'methods'

--- R/day-02.R
+++ R/day-02.R
@@ -13,6 +13,7 @@ submarine <- function(location = c(0, 0), aim = 0, mode = c("v1", "v2")) {
   program <- list()
   index <- 1
 
+  # "public methods"
   load_program <- function(file) {
     instructions <- readLines(file) |>
       lapply(\(instruction) {
@@ -21,31 +22,6 @@ submarine <- function(location = c(0, 0), aim = 0, mode = c("v1", "v2")) {
       })
     program <<- instructions
     invisible(NULL)
-  }
-
-  next_move_v1 <- function(direction, step) {
-    if (direction == "forward")
-      move <- c(step, 0)
-    else if (direction == "up")
-      move <- c(0, -step)
-    else if (direction == "down")
-      move <- c(0, step)
-    else
-      stop("Invalid direction", direction, call. = FALSE)
-    move
-  }
-
-  next_move_v2 <- function(direction, step) {
-    move <- c(0, 0)
-    if (direction == "forward")
-      move <- c(step, abs(aim * step))
-    else if (direction == "up")
-      aim <<- aim + step
-    else if (direction == "down")
-      aim <<- aim - step
-    else
-      stop("Invalid direction", direction, call. = FALSE)
-    move
   }
 
   next_instruction <- function() {
@@ -79,8 +55,35 @@ submarine <- function(location = c(0, 0), aim = 0, mode = c("v1", "v2")) {
 
   get_location <- function() location
 
+  # "private methods"
+  next_move_v1 <- function(direction, step) {
+    if (direction == "forward")
+      move <- c(step, 0)
+    else if (direction == "up")
+      move <- c(0, -step)
+    else if (direction == "down")
+      move <- c(0, step)
+    else
+      stop("Invalid direction", direction, call. = FALSE)
+    move
+  }
+
+  next_move_v2 <- function(direction, step) {
+    move <- c(0, 0)
+    if (direction == "forward")
+      move <- c(step, abs(aim * step))
+    else if (direction == "up")
+      aim <<- aim + step
+    else if (direction == "down")
+      aim <<- aim - step
+    else
+      stop("Invalid direction", direction, call. = FALSE)
+    move
+  }
+
   next_move <- if (mode == "v1") next_move_v1 else next_move_v2
 
+  # return the constructed "object" as a list of its methods
   invisible(list(
     load_program = load_program,
     next_instruction = next_instruction,

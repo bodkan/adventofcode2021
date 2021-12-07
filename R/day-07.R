4bb9ca366784484a70a1afe9e3e8c3428c362679
Martin Petr <git@bodkan.net>
2021-12-07 11:28:15 +0100

    Update comment

--- R/day-07.R
+++ R/day-07.R
@@ -2,14 +2,16 @@ read_crabs <- function(file) {
   scan(file, what = integer(), sep = ",", quiet = TRUE)
 }
 
-#' Compute the fuel requirements to move all crabs to one of the given
-#' final positions in a vectorized way
+#' Compute the fuel requirements to move all crabs to one of the given final
+#' positions in a vectorized way
 #'
 #' @param crabs Vector of integer positions of all crabs
 #' @param options Vector of potential final positions
 #' @param fuel_fun Function to transform the distances into fuel
+#'
+#' @return Vector of fuel requirements to get to each position among the
+#'   \code{options}
 compute_fuel <- function(crabs, options, fuel_fun = identity) {
-  # TIL about `outer()` and it is amazing
   colSums(fuel_fun(abs(outer(crabs, options, "-"))))
 }
 

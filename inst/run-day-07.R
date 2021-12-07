6e5d97842feae6c48cb36472222c632abe40f8dc
Martin Petr <git@bodkan.net>
2021-12-07 11:29:38 +0100

    Fix final run

--- inst/run-day-07.R
+++ inst/run-day-07.R
@@ -5,12 +5,12 @@ input_file <- system.file("extdata/day-07.txt", package = "adventofcode2021")
 crabs <- read_crabs(input_file)
 options <- seq(min(crabs), max(crabs))
 
-part1 <- compute_fuel(crabs, options)
+part1 <- min(compute_fuel(crabs, options))
 
 print_result(day = 7, part = 1, part1)
 check_answer(day = 7, part = 1, part1)
 
-part2 <- compute_fuel(crabs, options)
+part2 <- min(compute_fuel(crabs, options, fuel_fun = incremental_fuel))
 
 print_result(day = 7, part = 2, part2)
 check_answer(day = 7, part = 2, part2)

e7af3a45a86f6351c5186561e49fd9087c6c8b8c
Martin Petr <git@bodkan.net>
2021-12-12 16:47:25 +0100

    Reorganize day 12 code

--- R/day-12.R
+++ R/day-12.R
@@ -1,39 +1,4 @@
-# First condition for traversing the graph
-can_enter1 <- function(node, visits) {
-  toupper(node) == node | !visits[node]
-}
-
-# Second condition for traversing the graph
-can_enter2 <- function(node, visits) {
-  toupper(node) == node |
-    !visits[node] |
-    (node != "start" &&
-       visits[node] == 1 &&
-       all(visits[!names(visits) %in% c("start", node) &
-                    names(visits) == tolower(names(visits))
-       ] != 2))
-}
-
-# Read the adjacency matrix from the input file of graph edges
-read_caves <- function(file) {
-  # read edges from the input file
-  edges <- readLines(file) |> strsplit("-")
-  # get nodes of the graph
-  nodes <- sort(unique(unlist(edges)))
-
-  # generate empty adjacency matrix ...
-  adj <- matrix(FALSE, nrow = length(nodes), ncol = length(nodes))
-  rownames(adj) <- colnames(adj) <- nodes
-  # ... and fill it accordingly
-  for (e in edges) {
-    adj[e[1], e[2]] <- TRUE
-    adj[e[2], e[1]] <- TRUE
-  }
-  adj
-}
-
 # Recursively visit each node of the graph assuming a given condition
-# function provided in `can_enter`
 visit <- function(node, adjacency, visits, path, result, condition) {
   if (node == "end") {
     result$paths <- append(result$paths, list(c(path, node)))
@@ -68,4 +33,38 @@ find_paths <- function(file, condition) {
   visit(node = "start", adjacency, visits, path = c(), result, condition)
 
   result$paths
+}
+
+# Read the adjacency matrix from the input file of graph edges
+read_caves <- function(file) {
+  # read edges from the input file
+  edges <- readLines(file) |> strsplit("-")
+  # get nodes of the graph
+  nodes <- sort(unique(unlist(edges)))
+
+  # generate empty adjacency matrix ...
+  adj <- matrix(FALSE, nrow = length(nodes), ncol = length(nodes))
+  rownames(adj) <- colnames(adj) <- nodes
+  # ... and fill it accordingly
+  for (e in edges) {
+    adj[e[1], e[2]] <- TRUE
+    adj[e[2], e[1]] <- TRUE
+  }
+  adj
+}
+
+# First condition for traversing the graph (part 1)
+can_enter1 <- function(node, visits) {
+  toupper(node) == node | !visits[node]
+}
+
+# Second condition for traversing the graph (part 2)
+can_enter2 <- function(node, visits) {
+  toupper(node) == node |
+    !visits[node] |
+    (node != "start" &&
+       visits[node] == 1 &&
+       all(visits[!names(visits) %in% c("start", node) &
+                    names(visits) == tolower(names(visits))
+       ] != 2))
 }

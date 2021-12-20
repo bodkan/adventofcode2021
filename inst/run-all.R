26282a839e9c158f87f9c3d7daab70fdb2bfacd4
Martin Petr <git@bodkan.net>
2021-12-20 11:01:27 +0100

    First stab at day 19

--- inst/run-all.R
+++ inst/run-all.R
@@ -1,3 +1,4 @@
+source("inst/run-day-19.R", local = TRUE)
 source("inst/run-day-18.R", local = TRUE)
 source("inst/run-day-17.R", local = TRUE)
 source("inst/run-day-16.R", local = TRUE)
---
+++ inst/run-day-19.R
@@ -0,0 +1,15 @@
+suppressMessages(devtools::load_all("."))
+
+input_file <- system.file("extdata/day-19.txt", package = "adventofcode2021")
+
+part1 <-
+
+print_result(day = 19, part = 1, part1)
+check_answer(day = 19, part = 1, part1)
+
+part2 <-
+
+print_result(day = 19, part = 2, part2)
+check_answer(day = 19, part = 2, part2)
+
+print_sep()

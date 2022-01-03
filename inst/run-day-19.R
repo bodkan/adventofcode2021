7a121b911054e980f83f1b1ce2e8d2d666dc31fb
Martin Petr <git@bodkan.net>
2022-01-03 21:47:08 +0100

    Implement day 19 part 2

--- inst/run-day-19.R
+++ inst/run-day-19.R
@@ -9,11 +9,16 @@ beacons <- dedupe_beacons(result$aligned)
 part1 <- nrow(beacons)
 
 print_result(day = 19, part = 1, part1)
-# check_answer(day = 19, part = 1, part1)
-#
-# part2 <-
-#
-# print_result(day = 19, part = 2, part2)
-# check_answer(day = 19, part = 2, part2)
-#
-# print_sep()
+check_answer(day = 19, part = 1, part1)
+
+locations <- do.call(rbind, result$locations)
+
+rel_distances <- compute_distances(locations)
+manhattan_distances <- rowSums(distances, dims = 2)
+
+part2 <- max(manhattan_distances)
+
+print_result(day = 19, part = 2, part2)
+check_answer(day = 19, part = 2, part2)
+
+print_sep()
